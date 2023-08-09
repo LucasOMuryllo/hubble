dbe39a5f69daf51988841191efd57e8b7c136395
Lucas <lumuryllo86@gmail.com>
2023-08-08 23:44:49 -0300

    feat: adjusting header

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,15 @@
+import Button from "@mui/material/Button";
 import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <div></div>
+        <div>
+            <p>OR SIGN UP WITH</p>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+        </div>
     )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,18 +4,18 @@ import React from "react";
 const Header: React.FC = () => {
     return (
         <div className="App-header">
-            <h1>Start Up</h1>
-            <p>Products</p>
-            <p>Pricing</p>
-            <p>Community</p>
+            <h1 className="header-title">Start Up</h1>
+            <a className="header-itens" href="#">Products</a>
+            <a className="header-itens" href="#">Pricing</a>
+            <a className="header-itens" href="#">Community</a>
 
-            <Button variant="text">
-                <p>Help</p>
-            </Button>
-            <Button variant="text">
-                <p>Login</p>
-            </Button>
-            <Button variant="outlined">
+            <a className="header-button" href="#">
+                Help
+            </a>
+            <a className="header-button" href="#">
+                Login
+            </a>
+            <Button className="button-sign" variant="outlined">
                 <p>Sign Up</p>
             </Button>
         </div>
